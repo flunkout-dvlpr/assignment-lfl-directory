5b7a87c661887142bd8f76ce4da508ef1ee7de2b
flunkout-dvlpr <info@juliogonzalez.space>
2022-02-17 17:18:31 -0500

    Create function to create table, add rows of employees and append to element with id card

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Your work here
+
+function employeesView () {
+  document.getElementById('card').innerHTML = '';
+  let employeesTable = document.createElement('table');
+  employeesTable.innerHTML = `
+    <tr>
+      <th>Name</th>
+      <th>Office</th>
+      <th>Phone #</th>
+    </tr>
+  `;
+  for (let idx=0; idx<employeeList.length; idx++) {
+    employeesTable.innerHTML += `
+      <tr>
+        <td>${employeeList[idx].name}</td>
+        <td>${employeeList[idx].officeNum}</td>
+        <td>${employeeList[idx].phoneNum}</td>
+      </tr>
+    `;
+  }
+  document.getElementById('card').appendChild(employeesTable);
+}
